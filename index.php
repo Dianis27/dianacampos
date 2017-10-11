fdfd250db0f9ddef879c3ac4870b3551b93167f8
Dianis27 <32026478+Dianis27@users.noreply.github.com>
2017-10-11 17:32:58 -0500

    h

---
+++ index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+	<title>Departamento de Cultura y Deportes </title>
+	<link rel="stylesheet" href="estilos.css">
+	<link rel="stylesheet" href="fonts.css">
+	<style type="text/css">
+	body {
+	background-color: #ccff99;
+}
+    </style>
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+</head>
+<body>
+	<header>
+		<div class="menu_bar">
+			<a href="#" class="bt-menu"><span class="icon-menu"></span>Departamento de Actividades Culturales y Deportivas</a>
+		</div>
+
+		<nav>
+			<ul>
+				<li><a href="#"><span class="#">Cultura y Deportes</span></a></li>
+				<li><a href="index.html"><span class="icon-home2"></span>Inicio</a></li>
+				<li><a href="informacion.php"><span class="icon-books"></span>Informacion</a></li>
+				<li><a href="altas.php"><span class="icon-pencil"></span>Registro</a></li>
+				<li><a href="login.php"><span class="icon-user"></span>Login</a></li>
+				
+
+
+			</ul>
+		</nav>
+	</header>
+
+	<section>
+
+<head>
+     <meta charset="uft-8">
+     <link rel="stylesheet" href="bootstrap/css/bootstrap-css">
+     <link rel="stylesheet" href="bootstrap/css/bootstrap-responsive.css">
+     <link rel="stylesheet" type="text/css" href="estilos/estilos.css"><center><h1>Bievenidos al Sistema de Actividades Culturales y Deportivas 
+</h1></center>
+</section>
+<center>
+	<script src="http://code.jquery.com/jquery-latest.js"></script><script src="menu.js"></script>
+	
+<head><center><u><b><h1><marquee align="middle">
+   
+<img src="css/ajedrez.jpg" width="700" height="450" alt="Img"/>
+<img src="css/rubik.jpg" width="700" height="450" alt="Img" />
+<img src="css/dibujo.jpg" width="700" height="450" alt="Img" />
+<img src="css/papel maché.jpg" width="700" height="450" alt="Img" />
+<img src="css/teatro.jpg" width="700" height="450" alt="Img" />
+<img src="css/musica.jpg" width="700" height="450" alt="Img" />
+<img src="css/zumba.jpg" width="700" height="450" alt="Img" />	
+<img src="css/taebo.jpg" width="700" height="450" alt="Img" />
+<img src="css/futbol.jpg" width="700" height="450" alt="Img" />
+<img src="css/basquetbol.jpg" width="700" height="450" alt="Img" />
+<img src="css/voleibol.jpg" width="700" height="450" alt="Img" />
+<img src="css/imagen.jpg" width="700" height="450" alt="Img" />
+
+   
+</marquee></h1></b></u></center></head>
+
+</body>
+</html>
+
+</center>
+<p>&nbsp;</p>
+
+</body>
+</html>
+
+<footer>
+        <div class='define'>
+
+        <center><font color="gray"><p>Tecnologico de Estudios Superiores de Ixtapaluca</p></font></center>            
+
+            <center><font color="gray"><p>CKm. 7 de la carretera Ixtapaluca-Coatepec s/n San Juan, Distrito de Coatepec, Ixtapaluca, Estado de México C.P.56580, Tel. ( 055 ) 13148152 ext. 129.</p></font></center>
+
+
+        </div>
+    </footer>
